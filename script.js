b45f8f24f2b8049b8a8f70886a4f01d605a11a36
faraz <sarfrazakbarjamali@gmail.com>
2026-03-13 02:22:38 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const convertBtn = document.getElementById("convert-btn")
+
+let lengthResultCard = document.getElementById("length-result-card")
+let volumeResultCard = document.getElementById("volume-result-card")
+let massResultCard = document.getElementById("mass-result-card")
+const inputValue = document.getElementById("input-el").value
+
+// FUNCTION TO CONVERT LENGTH  (FEET - METER)
+
+// FUNCTION TO CONVERT VOLUME (LITERS-GALLONE)
+
+// FUNCTION TO CONVERT MASS (KILOGRAMS - POUNDS)
